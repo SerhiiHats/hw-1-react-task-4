00edab23776d04cd0cbd276d64ae6309686afd5e
Serhi Hats <gatsserv@gmail.com>
2023-04-25 01:18:11 +0300

    prepare solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import logo_react from './images/logo_react.svg';
+import logo_html from './images/logo_html.svg';
+import logo_css3 from './images/logo_css3.svg';
+import github_mark_white from './images/github_mark_white.svg';
+import logo_javascript from './images/logo_javascript.svg';
+import logo_spring from './images/logo_spring.svg';
+import logo_java from './images/logo_java.svg';
+
+
+const arrImg = [logo_html, logo_css3, github_mark_white, logo_javascript, logo_react, logo_java, logo_spring];
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+MyLikeImage();
+
+function MyLikeImage() {
+  let index = 0;
+  let timeId;
+
+  timeId && clearInterval(timeId);
+
+  timeId = setInterval(() => {
+    index = (index + 1) % arrImg.length;
+    // return arrImg[index];
+    // console.log(arrImg[index]);
+    // console.log(index);
+
+    root.render(
+      <App name={arrImg[index]} />
+    )
+
+
+  }, 2000);
+
+}
